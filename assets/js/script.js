2646bd5dc6d76f716b90495473189271ce5472b4
mforte215 <mark.fortee@gmail.com>
2023-11-21 23:00:49 -0500

    added full password generation functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  let enteredPasswordLength;
   let validatedPasswordLength = false;
   while (!validatedPasswordLength) {
-    let enteredPasswordLength = prompt("Enter a number for the length of password (Between 8 and 128)");
-    console.log("LOGGING PASSOWRD");
-    console.log(enteredPasswordLength);
+    enteredPasswordLength = prompt("Enter a number for the length of password (Between 8 and 128)");
     if (enteredPasswordLength >= 8 && enteredPasswordLength <= 128) {
       validatedPasswordLength = true;
     } else if (enteredPasswordLength === null) {
@@ -19,19 +18,50 @@ function writePassword() {
     let answerUpperCaseUsage = confirm("Confirm usage of upper case letters");
     let answerNumericUsage = confirm("Confirm usage of numeric characters");
     let specialCharUsage = confirm("Confirm usage of special characters");
-    var password = passwordConstructor(enteredPasswordLength, answerLowerCaseUsage, answerUpperCaseUsage, answerNumericUsage, specialCharUsage);
+    var password = generatePassword(enteredPasswordLength, answerLowerCaseUsage, answerUpperCaseUsage, answerNumericUsage, specialCharUsage);
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
+
   }
-
-
-
-
 
 }
 
-const passwordConstructor = (length, lowerCase, upperCase, numeric, special) => {
-  //TO DO: FUNCTION
+const generatePassword = (psLength, lowerCase, upperCase, numeric, special) => {
+  const numericCharSet = "0123456789";
+  const lowerCaseCharSet = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const specialCharSet = "!@#$%^&*()";
+
+  let useableChars = "";
+
+  if (numeric) {
+    useableChars += numericCharSet
+  }
+
+  if (lowerCase) {
+    useableChars += lowerCaseCharSet
+  }
+
+  if (upperCase) {
+    useableChars += upperCaseCharSet
+  }
+  if (special) {
+    useableChars += specialCharSet
+  }
+
+  if (useableChars == "") {
+    console.log()
+    alert("You chose an empty password");
+    return "You chose an empty password";
+  }
+  else {
+    let thePassword = ""
+    for (let i = 0; i <= psLength; i++) {
+      let randomIndex = Math.floor(Math.random() * useableChars.length);
+      thePassword += useableChars.substring(randomIndex, randomIndex + 1);
+    }
+    return thePassword;
+  }
 }
 
 
